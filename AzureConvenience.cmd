cea88c1f3e25615bd2034b98eae38488101dc496
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-12-18 14:24:52 +0100

    a

---
+++ AzureConvenience.cmd
@@ -0,0 +1,25 @@
+
+REM Source: https://github.com/chgeuer/AzureConvenience
+
+
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
+Rundll32 iesetup.dll, IEHardenLMSettings
+Rundll32 iesetup.dll, IEHardenUser
+Rundll32 iesetup.dll, IEHardenAdmin
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /f /va
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /f /va
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "First Home Page" /f
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Default_Page_URL" /t REG_SZ /d "about:blank" /f
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d "about:blank" /f
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+chocolatey install sublimetext2
+start "" "d:\Program Files\Sublime Text 2\sublime_text.exe"
+
+chocolatey install sysinternals
+start "" d:\tools\sysinternals\Dbgview.exe /accepteula
+start "" d:\tools\sysinternals\procmon.exe /accepteula
+
+%windir%\system32\inetsrv\InetMgr.exe
