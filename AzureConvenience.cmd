86e784b8567eca3c17107ee2eadbc65c1813a19a
Christian <chgeuer@users.noreply.github.com>
2015-02-02 10:21:05 +0100

    Update AzureConvenience.cmd

--- AzureConvenience.cmd
+++ AzureConvenience.cmd
@@ -30,4 +30,4 @@ start "" %SystemDrive%\tools\sysinternals\procexp.exe /accepteula
 # ping, a.k.a. pause 2000 ms
 ping 1.1.1.1 -n 1 -w 2000 >NUL
 copy %tmp%\procexp64.exe %windir%\system32
-
+start "" %windir%\system32\procexp64.exe
